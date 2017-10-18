5f397e5c9b5ae20061df3dc44d55abbb95be58c7
Avds1198 <aymanvandersaar@hotmail.com>
2017-10-18 09:51:19 +0200

    first commit

--- src/main/java/com/ayman/messagingapp/Main.java
+++ src/main/java/com/ayman/messagingapp/Main.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.ayman.messagingapp;
 
-/**
- *
- * @author ayman
- */
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class Main {
+    @Value("${spring.datasource.url}")
+  private String dbUrl;
+    
+    @Autowired
+  private DataSource dataSource;
+
+    
+    public static void main(String[] args) throws Exception {
+    SpringApplication.run(Main.class, args);
+  }
+    
+    @RequestMapping("/")
+  String index() {
+    return "index";
+  }
     
 }
